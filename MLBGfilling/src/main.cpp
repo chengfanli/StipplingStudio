0b8bf815fae746c91291cffd9d5e5d7bd87e4fdb
Ahhhh2016 <zuiaixinyixuan@163.com>
2024-04-16 15:23:04 -0400

    initial push with new folder

---
+++ MLBGfilling/src/main.cpp
@@ -0,0 +1,67 @@
+#include "mainwindow.h"
+#include "wlbg.h"
+#include <cstdlib>
+#include <ctime>
+
+#include <QApplication>
+#include <QSurfaceFormat>
+#include <QScreen>
+#include <QLabel>
+#include "settings.h"
+
+#include <QCommandLineParser>
+#include <QtCore>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    srand(static_cast<unsigned>(time(0)));
+
+    // Create a Qt application
+    QApplication a(argc, argv);
+    QCoreApplication::setApplicationName("WLBG");
+    QCoreApplication::setOrganizationName("CS 2240");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+
+    // Set OpenGL version to 4.1 and context to Core
+//    QSurfaceFormat fmt;
+//    fmt.setVersion(4, 1);
+//    fmt.setProfile(QSurfaceFormat::CoreProfile);
+//    QSurfaceFormat::setDefaultFormat(fmt);
+
+    // Parse input
+    QCommandLineParser parser;
+    parser.addHelpOption();
+    parser.addPositionalArgument("config",  "Path of the config (.ini) file.");
+    parser.process(a);
+    const QStringList args = parser.positionalArguments();
+    if (args.size() < 1) {
+        std::cerr << "Not enough arguments. Please provide a path to a config file (.ini) as a command-line argument." << std::endl;
+        a.exit(1);
+        return 1;
+    }
+
+    // settings
+    QSettings input( args[0], QSettings::IniFormat);
+    // IO
+    settings.image_path = QString::fromStdString(input.value("IO/image_path").toString().toStdString());
+    settings.output_path = QString::fromStdString(input.value("IO/output_path").toString().toStdString());
+    // stipple init
+    settings.init_stipple_num = input.value("INIT/init_stipple_num").toInt();
+    settings.init_stipple_size = input.value("INIT/init_stipple_size").toFloat();
+    settings.supersampling_factor = input.value("INIT/supersampling_factor").toInt();
+    // wlbg para
+    settings.max_iteration = input.value("WLBG/max_iteration").toInt();
+    settings.hysteresis = input.value("WLBG/hysteresis").toFloat();
+    settings.hysteresis_delta = input.value("WLBG/hysteresis_delta").toFloat();
+    settings.adaptive_stipple_size = input.value("WLBG/adaptive_stipple_size").toBool();
+
+
+
+    // Create a GUI window
+    MainWindow w;
+
+    w.show();
+
+    return a.exec();
+}
---
+++ MLBGfilling/src/mainwindow.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QSlider>
+#include <QSpinBox>
+#include <QRadioButton>
+#include <QCheckBox>
+#include <QDoubleSpinBox>
+#include <QLabel>
+#include <QPushButton>
+#include <QBoxLayout>
+
+#include "canvas.h"
+
+class MainWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Canvas *m_canvas;
+    MainWindow();
+    ~MainWindow();
+
+private:
+    void setupCanvas2D();
+
+
+    void addHeading(QBoxLayout *layout, QString text);
+    void addLabel(QBoxLayout *layout, QString text);
+    void onClearButtonClick();
+    void onUploadButtonClick();
+    void onSaveButtonClick();
+    void setUIntVal(std::uint8_t &setValue, int newValue);
+    void setIntVal(int &setValue, int newValue);
+    void setFloatVal(float &setValue, float newValue);
+    void setBoolVal(bool &setValue, bool newValue);
+    void setupCanvas();
+    void addPushButton(QBoxLayout *layout, QString text, auto function);
+
+    void onStippleButtonClick();
+    void onDrawVoronoiButtonClick();
+};
